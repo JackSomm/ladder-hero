9ef0a92bb59d11991b76c913e02b727bf3810952
Jack Sommer <jack.somm1@gmail.com>
2020-08-03 21:28:46 -0600

    Organized data on individual replay page

---
+++ src/components/Snapshot.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-carousel-item>
+    <p>
+      Lost Minerals: {{ data.lostMinerals}}, Lost Vespene: {{ data.lostVespene}}
+    </p>
+    <p>
+      Unspent Minerals: {{ data.unspentMinerals}}, Unspent Vespene: {{ data.unspentVespene}}
+    </p>
+    <p>
+      Mineral Collection Rate: {{ data.collectionRateMinerals}}, Vespene Collection Rate: {{ data.collectionRateVespene}}
+    </p>
+    <p>
+      Active Workers: {{ data.activeWorkers}}
+    </p>
+    <p>
+      Army Value (Minerals): {{ data.armyValueMinerals}}, Army Value (Vespene): {{ data.armyValueVespene}}
+    </p>
+  </v-carousel-item>
+</template>
+
+<script>
+export default {
+  name: 'Snapshot',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+  },
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Replay.vue
+++ src/views/Replay.vue
@@ -1,15 +1,39 @@
 <template>
-  <v-main>
-    <h1>
-      {{ data }}
-    </h1>
-  </v-main>
+  <v-container>
+    <v-row>
+      <v-card
+        elevation="3"
+        width="48%"
+        v-for="player in data.players"
+        :key="player.name">
+        <h1>
+          {{ player.name }}
+        </h1>
+        <v-carousel
+          :continuous="false"
+          :cycle="cycle"
+          :show-arrows="true"
+          height="210"
+          hide-delimiters>
+          <Snapshot 
+            v-for="(snapshot, i) in player.snapshots"
+            :key="i"
+            :data="snapshot"/>
+        </v-carousel>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
+  import Snapshot from '../components/Snapshot.vue'
+
   export default {
     name: 'Replay',
+    components: {
+      Snapshot
+    },
     props: {
       slug: {
         type: String,
@@ -21,9 +45,6 @@ import axios from 'axios'
         data: ''
       }
     },
-    components: {
-      // Graphs, tables, etc.
-    },
     created() {
       axios
         .get(`https://ladder-hero-api.honnold.me/api/v1/replays/${this.slug}`)
@@ -34,5 +55,25 @@ import axios from 'axios'
 </script>
 
 <style scoped>
-
+h1 {
+  margin-bottom: 20px;
+}
+.v-card {
+  margin: 1rem;
+  padding: 2rem;
+  text-align: center;
+  background-color: #434c5e;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.v-window__container {
+  height: auto!important;
+}
+</style>
+<style>
+.v-window__next, .v-window__prev {
+  top: calc(33% - 20px);
+}
 </style>
