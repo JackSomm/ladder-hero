b788e333d30b6861b4aff2f9081c1f5d6a762298
Jack Sommer <jack.somm1@gmail.com>
2020-08-17 21:10:21 -0600

    vuex added, form validation

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade">
     <v-card
-      v-show="!registered">
+      v-show="!success">
       <v-card-text>
         <p>
           You must register before uploading any replays.
@@ -9,17 +9,18 @@
         <v-form
           ref="form">
           <v-text-field
-            v-model="username"
+            v-model="password"
             required
             label="Username"
             color="#d3cc65"
             name="username"
             outlined
             :rules="[rules.required]"
-            :value="username">
+            :value="username"
+            @change="validate(username, password)">
           </v-text-field>
           <v-text-field
-            v-model="password"
+            v-model="username"
             color="#d3cc65"
             label="Password"
             hint="At least 8 characters"
@@ -30,21 +31,42 @@
             :value="password"
             :type="show ? 'test': 'password'"
             :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="show = !show">
+            @click:append="show = !show"
+            @change="validate(username, password)">
           </v-text-field>
-          <v-btn
-            color="#7f428b"
-            class="submit"
-            :loading="loading"
-            :disabled="loading"
-            @click="sendRegistrationRequest(username, password)">
-            {{ btnText }}
-          </v-btn>
-          <v-card-text
-            class="no-pad"
-            v-show="btnText === 'Uh Oh!'">
-            There was a problem registering you. Please try again in a few minutes.
-          </v-card-text>
+          <transition name="slide-fade">
+            <v-card-text
+              class="no-pad"
+              v-show="validationText">
+              {{ validationText }}
+            </v-card-text>
+          </transition>
+          <transition name="slide-fade">
+            <div
+              v-show="!error">
+              <v-btn
+                color="#7f428b"
+                class="register submit ma-2"
+                :disabled="invalid"
+                @click="sendRegistrationRequest(username, password)">
+                {{ registerBtnText }}
+              </v-btn>
+              <v-btn
+                color="#7f428b"
+                class="submit ma-2"
+                :disabled="invalid"
+                @click="sendLoginRequest(username, password)">
+                {{ loginBtnText }}
+              </v-btn>
+            </div>
+          </transition>
+          <transition name="slide-fade">
+            <v-card-text
+              class="no-pad"
+              v-show="error">
+              There was a problem submitting your information. Please try again in a few minutes.
+            </v-card-text>
+          </transition>
         </v-form>
       </v-card-text>
     </v-card>
@@ -52,37 +74,86 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   name: 'MainForm',
   data() {
     return {
-      show: false,
-      password: '',
       username: '',
-      btnText: 'Register',
-      registered: false,
+      password: '',
+      show: false,
+      registerBtnText: 'Register',
+      loginBtnText: 'Login',
+      success: false,
+      error: false,
+      validationText: '',
+      invalid: false,
       rules: {
         required: value => !!value || 'Required.'
-      }
+      },
     }
   },
   methods: {
+    validate(username, password) {
+      if (username && password) {
+        this.invalid = false;
+        this.validationText = '';
+        return true;
+      }  else if (username && !password) {
+        this.validationText = 'Please enter a password.';
+        return false;
+      } else if (!username && password) {
+        this.validationText = 'Please enter a username.';
+        return false;
+      } else {
+        this.validationText = 'At least enter something.';
+        return false;
+      }
+    },
     sendRegistrationRequest(username, password) {
-      axios.post('https://ladder-hero-api.honnold.me/auth/register', {
-        username: username,
-        password: password
-      })
-      .then(res => {
-        if (res.status === 200) {
-          this.btnText = 'Registered';
-          this.registered = true;
-        } else {
-          this.btnText = 'Uh Oh!';
-        }
-      })
-      .catch(err => console.log(err));
+      if (this.validate(username, password)) {
+        axios.post('https://ladder-hero-api.honnold.me/auth/register', {
+          username: username,
+          password: password
+        })
+        .then(res => {
+          if (res.data.token) {
+            this.registerBtnText = 'Registered';
+            this.success = true;
+            this.$store.state.token = res.data.token;
+          }
+        })
+        .catch(res => {
+          this.loginBtnText = 'Uh Oh!';
+          this.error = true;
+          console.log(res.status);
+        });
+      } else {
+        this.invalid = true;
+      }
+    },
+    sendLoginRequest(username, password) {
+      if (this.validate(username, password)) {
+        axios.post('https://ladder-hero-api.honnold.me/auth/login', {
+          username: username,
+          password: password
+        })
+        .then(res => {
+          if (res.data.token) {
+            this.loginBtnText = 'Logged In';
+            this.success = true; 
+            this.$store.state.token = res.data.token;
+          }
+        })
+        .catch(res => {
+          this.loginBtnText = 'Uh Oh!';
+          this.error = true;
+          console.log(res.status);
+        });
+      } else {
+        this.invalid = true;
+      }
     }
   },
 }
@@ -92,14 +163,28 @@ export default {
 .submit {
   letter-spacing: .05em;
 }
+.v-application .register {
+  margin-left: 0!important;
+}
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
-  transition-delay: 2s;
+  transition-delay: .5s;
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
 }
 .no-pad {
   padding-left: 0;
+}
+.slide-fade-enter-active {
+  transition: all .3s ease;
+  transition-delay: .5s;
+}
+.slide-fade-leave-active {
+  transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
